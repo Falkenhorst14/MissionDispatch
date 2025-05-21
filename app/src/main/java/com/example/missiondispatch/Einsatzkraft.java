4f93fd893b0dd11257194acf92c7789058ff5ab7
Lukas <77630523+Falkenhorst14@users.noreply.github.com>
2025-05-21 10:57:58 +0200

    Added class Einsatzkraft.java

---
+++ app/src/main/java/com/example/missiondispatch/Einsatzkraft.java
@@ -0,0 +1,18 @@
+package com.example.missiondispatch;
+
+public class Einsatzkraft {
+
+    private int id;
+    private String vorname;
+    private String nachname;
+    private String telefon;
+
+    //Qualifikationen
+    private int tauchAusbildung;
+    private int bootsAusbildung;
+    private int wrdAusbildung;
+    private int sanAusbildung;
+    private int funkAusbildung;
+    private int fuehrungsAusbildung;
+
+}
