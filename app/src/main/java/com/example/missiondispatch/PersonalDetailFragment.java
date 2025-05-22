a816c50a89bb3284790310f3d0ffd03793461500
Lukas <77630523+Falkenhorst14@users.noreply.github.com>
2025-05-22 02:32:53 +0200

    Anzeige von uebergebenen Detaildaten zu Personal

--- app/src/main/java/com/example/missiondispatch/PersonalDetailFragment.java
+++ app/src/main/java/com/example/missiondispatch/PersonalDetailFragment.java
@@ -2,11 +2,17 @@ package com.example.missiondispatch;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +29,20 @@ public class PersonalDetailFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Einsatzkraft einsatzkraft;
+    private TextView tvName;
+    private TextView tvFuehrungsausbildung;
+    private TextView tvFuehrungsausbildungSub;
+
+    private CheckBox checkbxImEinsatz;
+    private TextView tvTelefon;
+    private TextView tvTauchausbildung;
+    private TextView tvBootsausbildung;
+    private TextView tvStroemungsrettungsausbildung;
+    private TextView tvWrdausbildung;
+    private TextView tvSanausbildung;
+    private TextView tvFunkausbildung;
 
     public PersonalDetailFragment() {
         // Required empty public constructor
@@ -53,6 +73,7 @@ public class PersonalDetailFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
@@ -61,4 +82,42 @@ public class PersonalDetailFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_personal_detail, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupViewElements(view);
+    }
+
+    private void setupViewElements(@NonNull View view) {
+        tvName = view.findViewById(R.id.tvpersonalName);
+        tvFuehrungsausbildung = view.findViewById(R.id.tvfuehrungsausbildung);
+        tvFuehrungsausbildungSub = view.findViewById(R.id.tvfuehrungSub);
+        checkbxImEinsatz = view.findViewById(R.id.checkboxEingesetzt);
+        tvTelefon = view.findViewById(R.id.tvtelefon);
+        tvTauchausbildung = view.findViewById(R.id.tvausbildungTauchen);
+        tvBootsausbildung = view.findViewById(R.id.tvausbildungBoot);
+        tvStroemungsrettungsausbildung = view.findViewById(R.id.tvausbildungStroemungsrettung);
+        tvWrdausbildung = view.findViewById(R.id.tvausbildungWasserrettung);
+        tvSanausbildung = view.findViewById(R.id.tvausbildungMedizin);
+        tvFunkausbildung = view.findViewById(R.id.tvausbildungFunk);
+
+        einsatzkraft = new Einsatzkraft(0,"Thomas", "Meier",
+                "04232 25293", 3, 1, 1,
+                1, 2, 2, "❚");
+        //●
+
+        tvName.setText(einsatzkraft.getVorname() + " " + einsatzkraft.getNachname());
+        tvFuehrungsausbildung.setText(einsatzkraft.getFuehrungsAusbildung().toString());
+        tvFuehrungsausbildungSub.setText(einsatzkraft.getFuehrungsausbildungString(einsatzkraft.getFuehrungsAusbildung()));
+        checkbxImEinsatz.setChecked(einsatzkraft.getImEinsatz());
+        tvTelefon.setText(einsatzkraft.getTelefon());
+        tvTauchausbildung.setText(einsatzkraft.getTauchausbildungString(einsatzkraft.getTauchAusbildung()));
+        tvBootsausbildung.setText(String.valueOf(einsatzkraft.getBootsAusbildung()));
+        tvStroemungsrettungsausbildung.setText(String.valueOf(einsatzkraft.getStroemungsrettungsAusbildung()));
+        tvWrdausbildung.setText(String.valueOf(einsatzkraft.getWrdAusbildung()));
+        tvSanausbildung.setText(String.valueOf(einsatzkraft.getSanAusbildung()));
+        tvFunkausbildung.setText(String.valueOf(einsatzkraft.getFunkAusbildung()));
+    }
+
 }
